c36ee01d7b3dab5add776a27b46c1d4302e25443
Luca <gaglia@gmail.com>
2025-01-20 12:45:22 +0100

    Update start_docker.sh

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -1,1 +1,6 @@
-docker compose run --service-ports --rm gsm_repro
+if [ -z "$(docker images -q lucagaglia/gsm_repro 2> /dev/null)" ]; then
+  # If uncommented, this will build the image locally
+  #docker build -t gsm_repro .
+  docker pull lucagaglia/gsm_repro
+fi
+docker run -it --gpus all -p 8888:8888 --mount type=bind,source="$(pwd)/../",target=/home/app lucagaglia/gsm_repro
