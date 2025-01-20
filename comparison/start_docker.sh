9769d645687ee2f613596467e5af12a1156bf526
Luca <gaglia@gmail.com>
2025-01-20 12:39:01 +0100

    Update start_docker.sh

--- comparison/start_docker.sh
+++ comparison/start_docker.sh
@@ -1,4 +1,6 @@
-if [ -z "$(docker images -q gsm_comparison 2> /dev/null)" ]; then
-  docker build -t gsm_comparison .
+if [ -z "$(docker images -q lucagaglia/gsm_comparison 2> /dev/null)" ]; then
+  # If uncommented, this will build the image locally
+  #docker build -t gsm_comparison .
+  docker pull lucagaglia/gsm_comparison
 fi
-docker run -it --gpus all --mount type=bind,source="$(pwd)/../",target=/home/app gsm_comparison
+docker run -it --gpus all --mount type=bind,source="$(pwd)/../",target=/home/app lucagaglia/gsm_comparison
